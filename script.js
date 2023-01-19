a2fb0534ef840ee59aeab654c826015c5adb41ae
sevillea <aseville92@gmail.com>
2023-01-19 20:37:52 +0000

    added-starter-&-psuedo

---
+++ script.js
@@ -0,0 +1,14 @@
+// Work Day Scheduler created using jQuery, JavaScript, Moment.js, HTML & CSS. 
+// Add timeblocks in hourly increments from 9am - 5pm. 
+// Each timeblock will have a text entry box for the user to input a task.
+// Each inputted task will save to local storage.
+// Each timeblock will have an 'add event' button on the right hand side. 
+// Each add event button will use event listener 'click'. 
+
+
+// Moment JavaScript for  variable 'timeNow' to show present time. 
+// Need a variable for timeblocks. 
+// Need a variable for current date to display at top of calender. 
+// Each timeblock will need corresponding HTML for color change relative to event state (below).
+
+// I will need a function to check each hour block to see if the calendar event is in the past, present or future
