d6444135f9c2fb922c9adcf5a428dab152ceb72c
sevillea <aseville92@gmail.com>
2023-01-20 19:30:33 +0000

    added-timeblocks-save-Btn

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 // Work Day Scheduler created using jQuery, JavaScript, Moment.js, HTML & CSS. 
+// I will need to add a dynamic date at the top of the page to reflect the current date using moment.js. 
 // Add timeblocks in hourly increments from 9am - 5pm. 
 // Each timeblock will have a text entry box for the user to input a task.
+// Each box will be color coded.  
 // Each inputted task will save to local storage.
 // Each timeblock will have an 'add event' button on the right hand side. 
-// Each add event button will use event listener 'click'. 
+// Each add event button will use event listener 'click' when the user clicks, using jQuery. 
 
 
-// Moment JavaScript for  variable 'timeNow' to show present time. 
+// Moment JavaScript for  variable 'timeNow' to show present time at top of screen. 
 // Need a variable for timeblocks. 
 // Need a variable for current date to display at top of calender. 
 // Each timeblock will need corresponding HTML for color change relative to event state (below).
 
-// I will need a function to check each hour block to see if the calendar event is in the past, present or future
 
-// I'll need HTML Bootstrap grid containers for my save buttons.
-// I'll need a Bootstrap save button for each time and task
-// Each row, with its own time, will have a <textarea> in HTML to house an id, placeholder, class, rows and columns. 
+// I will need a function to check each hour block to see if the calendar event is in the past, present or future
+// PAs, present of future will be color coded according to what is past, what is present, what is to come. 
+
+// I'll need HTML Bootstrap grid containers for my time and text boxes. 
+// I'll need a Bootstrap save button for each time and task.
+// Each row, with its own time, will have a <textarea> in HTML to house an id, placeholder, class, rows and columns to style. 
 
 // I'll want to tailor my Bootstrap code to make the app more personable. 
