599d7398b674fdf9c0e6b0f567a67094e74fcd04
sevillea <aseville92@gmail.com>
2023-01-20 19:55:42 +0000

    timeblock-currentTime-variable

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // Work Day Scheduler created using jQuery, JavaScript, Moment.js, HTML & CSS. 
-// I will need to add a dynamic date at the top of the page to reflect the current date using moment.js. 
-// Add timeblocks in hourly increments from 9am - 5pm. 
-// Each timeblock will have a text entry box for the user to input a task.
+
+// I will need to add a dynamic date at the top of the page to reflect the current date using moment.js. DONE
+// Add timeblocks in hourly increments from 9am - 5pm. (HTML) DONE
+// Each timeblock will have a text entry box for the user to input a task. (HTML) DONE
 // Each box will be color coded.  
 // Each inputted task will save to local storage.
 // Each timeblock will have an 'add event' button on the right hand side. 
 // Each add event button will use event listener 'click' when the user clicks, using jQuery. 
 
 
-// Moment JavaScript for  variable 'timeNow' to show present time at top of screen. 
+// Moment JavaScript for  variable 'timeNow' to show present time at top of screen. DONE
 // Need a variable for timeblocks. 
 // Need a variable for current date to display at top of calender. 
 // Each timeblock will need corresponding HTML for color change relative to event state (below).
@@ -22,3 +23,16 @@
 // Each row, with its own time, will have a <textarea> in HTML to house an id, placeholder, class, rows and columns to style. 
 
 // I'll want to tailor my Bootstrap code to make the app more personable. 
+
+// Variable for current time and date:
+var timeNow = moment().format("MMMM Do YYYY");
+$("#currentDay").text(timeNow)
+
+// Variable for timeblocks. 
+var timeBlock = $(".hour");
+var now = parseInt(moment().format("H"));
+
+// Function for past present future timeblock:
+
+
+
