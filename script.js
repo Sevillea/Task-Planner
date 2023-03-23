6f504fafecc06dd7dc4bd66e537adf349a0c825f
sevillea <aseville92@gmail.com>
2023-03-23 13:44:15 +0000

    input-functions

--- script.js
+++ script.js
@@ -68,31 +68,13 @@ $(document).ready(function (){
     }
 ;});
 
- // Eventlistener for save buttons when user clicks 'save'.
-
-  $(saveBtn).click(function(event){
-    event.preventDefault();
-    var calendarItem =
-      saveBtn.target.parentElement.previousElementSibling.children[0].value;
-      // Commit to local storage
-      localStorage.setItem(saveBtn.target.attributes[0].value, textArea);
-  });
-
-
+ 
   // Clear input fields on click.
+
   function ClearFields() {
     document.getElementsById("planner-item").value = "";
   }
-
-  // Commit schedule entries to local storage
-  // $(document).ready(function () {
-  //   if (localStorage["9am"] !== null && localStorage["9am"] !== undefined) {
-  //     var nineAm = $("<p>" + localStorage["9am"] + "</p>");
-  //     $("#nineAM").append(nineAm[0].innerText);
-  //   } else {
-  //     ("");
-  //   }
-  // });
+  
 
   function add_local(){
     window.localStorage["planner-item"] = document.getElementsByClassName("planner-item").value;
@@ -102,8 +84,75 @@ $(document).ready(function (){
     document.getElementsByClassName("planner-item").value = window.localStorage["planner-item"];
   }
 
-  $(btn-secondary).click(function){
-    console.log(add_local);
+  // Eventlistener for save buttons when user clicks 'save'.
+
+  $(saveBtn).click(function(event){
+    event.preventDefault();
+    var calendarItem =
+      saveBtn.target.parentElement.previousElementSibling.children[0].value;
+      // Commit to local storage
+      localStorage.setItem(saveBtn.target.attributes[0].value, textArea);
+  },
+  console.log(calendarItem)
+  );
+
+  $(document).ready(function () {
+  if (localStorage["9am"] !== null && localStorage["9am"] !== undefined) {
+    var nineAm = $("<p>" + localStorage["9am"] + "</p>");
+    $("#nineAm").append(nineAm[0].innerText);
+  } else {
+    ("");
   }
+  if (localStorage["10am"] !== null && localStorage["10am"] !== undefined) {
+    var tenAm = $("<p>" + localStorage["10am"] + "</p>");
+    $("#tenAm").append(tenAm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["11am"] !== null && localStorage["11am"] !== undefined) {
+    var elevenAm = $("<p>" + localStorage["11am"] + "</p>");
+    $("#elevenAm").append(elevenAm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["12pm"] !== null && localStorage["12pm"] !== undefined) {
+    var twelvePm = $("<p>" + localStorage["12pm"] + "</p>");
+    $("#twelvePm").append(twelvePm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["1pm"] !== null && localStorage["1pm"] !== undefined) {
+    var onePm = $("<p>" + localStorage["1pm"] + "</p>");
+    $("#onePm").append(onePm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["2pm"] !== null && localStorage["2pm"] !== undefined) {
+    var twoPm = $("<p>" + localStorage["2pm"] + "</p>");
+    $("#twoPm").append(twoPm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["3pm"] !== null && localStorage["3pm"] !== undefined) {
+    var threePm = $("<p>" + localStorage["3pm"] + "</p>");
+    $("#threePm").append(threePm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["4pm"] !== null && localStorage["4pm"] !== undefined) {
+    var fourPm = $("<p>" + localStorage["4pm"] + "</p>");
+    $("#fourPm").append(fourPm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["5pm"] !== null && localStorage["5pm"] !== undefined) {
+    var fivePm = $("<p>" + localStorage["5pm"] + "</p>");
+    $("#fivePm").append(fivePm[0].innerText);
+  } else {
+    ("");
+  }
+});
+
+
 
 
